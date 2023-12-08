61803172fcb4c2962877ceffa61e61b23f43e5d2
johanleo97 <johan.chan@suizoamericano.edu.gt>
2023-12-07 19:31:01 -0600

    Limpiar js

--- js/index.js
+++ js/index.js
@@ -5,38 +5,4 @@ function index() {
     currentDateElement.textContent = "© " + formattedDate + " Optimum Star.";
 }
 
-function fetchin() {
-    fetch('../json/services.json')
-        .then(function (res) {
-            return res.json();
-        })
-        .then(function (data) {
-            var htmlstr = '';
-            var servicesbody = document.getElementById("servicesbody");
-            for(let i = 0; i < data.services.length; i++){
-                
-                
-                
-                htmlstr += '    <div class="card mb-3" style="max-width: 100%; height: 40vh;">'+
-                '<div class="row g-0">'+
-                  '<div class="col-md-4">'
-                    '<img src="..." class="img-fluid rounded-start" alt="...">'+
-                  '</div>'+
-                  '<div class="col-md-8">'+
-                    '<div class="card-body">'+
-                      '<h5 class="card-title">Card title</h5>'+
-                      '<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>'+
-                      '<p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>'+
-                    '</div>'+
-                  '</div>'+
-                '</div>'+
-              '</div>';
-               
-            }
-            servicesbody.innerHTML = htmlstr; 
-        })
-}
-
-
 index();
-//fetchin();
